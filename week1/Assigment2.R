7b5c004fcecde687d2f7947ab138a072d89902fb
WSR13990 <wahyusejatiroso@yahoo.co.id>
2017-03-27 22:02:42 +0700

    Workinng on assigment 2 week 1

---
+++ week1/Assigment2.R
@@ -0,0 +1,26 @@
+setwd('C:/Users/User/Downloads')
+IBM = read.csv('IBMStock.csv')
+GE = read.csv('GEStock.csv')
+ProcterGamble = read.csv('ProcterGambleStock.csv')
+CocaCola = read.csv('CocaColaStock.csv')
+Boeing = read.csv('BoeingStock.csv')
+
+IBM$Date = as.Date(IBM$Date, "%m/%d/%y")
+GE$Date = as.Date(GE$Date, "%m/%d/%y")
+CocaCola$Date = as.Date(CocaCola$Date, "%m/%d/%y")
+ProcterGamble$Date = as.Date(ProcterGamble$Date, "%m/%d/%y")
+Boeing$Date = as.Date(Boeing$Date, "%m/%d/%y")
+
+plot(IBM$Date[301:432], IBM$StockPrice[301:432], ylim = range(0:250), type = 'l', col = 'red', lwd = 2)
+lines(GE$Date[301:432], GE$StockPrice[301:432], col = 'blue', lwd = 2)
+lines(CocaCola$Date[301:432],CocaCola$StockPrice[301:432], col = 'orange', lwd = 2)
+lines(ProcterGamble$Date[301:432], ProcterGamble$StockPrice[301:432], col = 'black', lwd = 2)
+lines(Boeing$Date[301:432], Boeing$StockPrice[301:432], col = 'green', lwd = 2)
+
+abline(v=as.Date(c("2004-01-01")))
+abline(v=as.Date(c("2005-12-31")))
+
+mean(IBM$StockPrice)
+sort(tapply(IBM$StockPrice, months(IBM$Date), mean))
+sort(tapply(GE$StockPrice, months(GE$Date), mean))
+sort(tapply(CocaCola$StockPrice, months(CocaCola$Date), mean))
