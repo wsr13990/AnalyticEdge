3b6f73936da8366c9ac748bfc2d66fec2f0cd8de
WSR13990 <wahyusejatiroso@yahoo.co.id>
2017-03-27 22:20:17 +0700

    Working on Assigment 3 Week 1

---
+++ week1/Assigment3.R
@@ -0,0 +1,8 @@
+setwd('C:/Users/User/Downloads')
+CPS = read.csv('CPSData.csv')
+str(CPS)
+summary(CPS)
+
+sort(table(CPS$State))
+nrow(subset(CPS, CPS$Citizenship == 'Citizen, Native'|CPS$Citizenship == 'Citizen, Naturalized'))/nrow(CPS)
+table(CPS$Race, CPS$Hispanic)
