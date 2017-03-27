8f382320dee58ba925c4a309d9da2f17045d4902
WSR13990 <wahyusejatiroso@yahoo.co.id>
2017-03-27 23:50:44 +0700

    Finished Assigment 3 Week 3

--- week1/Assigment3.R
+++ week1/Assigment3.R
@@ -14,5 +14,12 @@ sort(tapply(is.na(CPS$MetroAreaCode), CPS$State, mean))
 
 MetroAreaMap = read.csv('MetroAreaCodes.csv')
 countryMap = read.csv('CountryCodes.csv')
+
 CPS = merge(CPS, MetroAreaMap, by.x="MetroAreaCode", by.y="Code", all.x=TRUE)
 sort(table(CPS$MetroArea))
+sort(tapply(CPS$Race == 'Asian', CPS$MetroArea, mean))
+sort(tapply(CPS$Education == "No high school diploma", CPS$MetroArea, mean, na.rm=TRUE))
+
+CPS = merge(CPS, countryMap, by.x = 'CountryOfBirthCode', by.y = 'Code', all.x = TRUE)
+tapply(CPS$Country != 'United States', CPS$MetroArea == "New York-Northern New Jersey-Long Island, NY-NJ-PA" , mean, na.rm = TRUE)
+sort(tapply(CPS$Country == 'Somalia', CPS$MetroArea, sum, na.rm = TRUE))
