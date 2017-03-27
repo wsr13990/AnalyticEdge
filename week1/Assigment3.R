f4534f9741924e497ab2cdb35332c66526660915
WSR13990 <wahyusejatiroso@yahoo.co.id>
2017-03-27 23:30:16 +0700

    Working on Assigment 3 Week 1

--- week1/Assigment3.R
+++ week1/Assigment3.R
@@ -6,3 +6,13 @@ summary(CPS)
 sort(table(CPS$State))
 nrow(subset(CPS, CPS$Citizenship == 'Citizen, Native'|CPS$Citizenship == 'Citizen, Naturalized'))/nrow(CPS)
 table(CPS$Race, CPS$Hispanic)
+
+table(CPS$Age, is.na(CPS$Married))
+table = table(CPS$Region, is.na(CPS$MetroAreaCode))
+
+sort(tapply(is.na(CPS$MetroAreaCode), CPS$State, mean))
+
+MetroAreaMap = read.csv('MetroAreaCodes.csv')
+countryMap = read.csv('CountryCodes.csv')
+CPS = merge(CPS, MetroAreaMap, by.x="MetroAreaCode", by.y="Code", all.x=TRUE)
+sort(table(CPS$MetroArea))
