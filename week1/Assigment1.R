ba056f965b4c10b5cb64b691f33c1b92f47925b0
unknown <wahyuros@PB90048.telkomsel.co.id>
2017-03-27 09:51:52 +0700

    Completed Week 1Assigment 1

--- week1/Assigment1.R
+++ week1/Assigment1.R
@@ -7,3 +7,11 @@ mvt$Weekday = weekdays(DateConvert)
 mvt$Date = DateConvert
 hist(mvt$Date, breaks=100)
 boxplot(Date ~ Arrest, data = mvt)
+Top5 = subset(mvt,
+              LocationDescription == 'STREET'|
+              LocationDescription == 'PARKING LOT/GARAGE(NON.RESID.)'|
+              LocationDescription == 'STREET'|
+              LocationDescription == 'ALLEY'|
+              LocationDescription == 'GAS STATION'|
+              LocationDescription == 'DRIVEWAY - RESIDENTIAL'
+              )
