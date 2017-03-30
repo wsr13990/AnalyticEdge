b5a9527d704890a47bf5e7ac28445dcc5c031fbd
WSR13990 <wahyusejatiroso@yahoo.co.id>
2017-03-30 22:37:19 +0700

    Finished Assigment 3 Week 3

--- week2/assigment3.R
+++ week2/assigment3.R
@@ -1,8 +1,40 @@
-setwd('C:/Users/wahyuros/Downloads')
-flu = read.csv('FluTrain.csv')
+library(zoo)
+setwd('C:/Users/User/Downloads')
+train = read.csv('fluTrain.csv')
 
-flu$Week[which.max(flu$ILI)]
-flu$Week[which.max(flu$Queries)]
+train$Week[which.max(train$ILI)]
+train$Week[which.max(train$Queries)]
 
-hist(flu$ILI)
-plot(log(flu$ILI), flu$Queries)
+hist(train$ILI)
+plot(log(train$ILI), train$Queries)
+model = lm(log(ILI) ~ Queries, data = train)
+summary(model)
+cor(log(train$ILI), train$Queries)
+
+test = read.csv('FluTest.csv')
+predTest = exp(predict(model, newdata = test))
+predTest[which(test$Week == '2012-03-11 - 2012-03-17')]/
+
+(test$ILI[which(test$Week == '2012-03-11 - 2012-03-17')] - predTest[which(test$Week == '2012-03-11 - 2012-03-17')])/test$ILI[which(test$Week == '2012-03-11 - 2012-03-17')]
+SSE = sum((test$ILI - predTest)^2)
+RMSE = sqrt(SSE/nrow(test))
+
+test$Week
+
+ILILag2 = lag(zoo(train$ILI), -2, na.pad=TRUE)
+train$ILILag2 = coredata(ILILag2)
+plot(log(train$ILILag2), log(train$ILI))
+
+model2 = lm(log(ILI) ~ Queries + log(ILILag2), data = train)
+summary(model2)
+
+ILILag2 = lag(zoo(test$ILI), -2, na.pad=TRUE)
+test$ILILag2 = coredata(ILILag2)
+summary(test)
+
+test$ILILag2[1] = train$ILI[nrow(train)-1]
+test$ILILag2[2] = train$ILI[nrow(train)]
+
+predTest2 = exp(predict(model2, newdata = test))
+SSE2 = sum((test$ILI - predTest2)^2)
+RMSE2 = sqrt(SSE2/nrow(test))
