d440c5a0e63c6506c312c2e248f49ae0f4bd7e88
unknown <WahyuRos@PB90048.telkomsel.co.id>
2017-03-30 16:10:37 +0700

    Finished Assigment2 Week2

--- week2/assigment2.R
+++ week2/assigment2.R
@@ -12,3 +12,12 @@ table(train$raceeth)
 train$raceeth = relevel(train$raceeth, 'White')
 test$raceeth = relevel(test$raceeth, 'White')
 
+model = lm(readingScore ~ ., data = train)
+summary(model)
+prediction = predict(model, newdata = test)
+SSE = sum((test$readingScore - prediction)^2)
+RMSE = sqrt(SSE/nrow(test))
+RMSE
+
+SST = sum((test$readingScore - mean(train$readingScore))^2)
+R2 = 1- SSE/SST
