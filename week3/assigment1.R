afc5e995025b585169eb7a7322c10e76fdb48134
unknown <WahyuRos@PB90048.telkomsel.co.id>
2017-04-01 14:35:39 +0700

    Completed Assigment1 Week 3

--- week3/assigment1.R
+++ week3/assigment1.R
@@ -6,5 +6,25 @@ MichaelJackson$songtitle[which(MichaelJackson$Top10 == 1)]
 table(songs$timesignature)
 songs$songtitle[which(songs$tempo == max(songs$tempo))]
 
-songsTrain = subset(songs, songs$year <= 2009)
-songsTest = subset(songs, songs$year == 2010)
+SongsTrain = subset(songs, songs$year <= 2009)
+SongsTest = subset(songs, songs$year == 2010)
+
+nonvars = c("year", "songtitle", "artistname", "songID", "artistID")
+SongsTrain = SongsTrain[ , !(names(SongsTrain) %in% nonvars) ]
+SongsTest = SongsTest[ , !(names(SongsTest) %in% nonvars) ]
+
+SongsLog1 = glm(Top10 ~., data = SongsTrain, family = binomial)
+summary(SongsLog1)
+
+cor(SongsTrain$energy, SongsTrain$loudness)
+SongsLog2 = glm(Top10 ~ . - loudness, data=SongsTrain, family=binomial)
+summary(SongsLog2)
+
+SongsLog3 = glm(Top10 ~ . - energy, data=SongsTrain, family=binomial)
+summary(SongsLog3)
+
+SongPredict = predict(SongsLog3, newdata = SongsTest, type ='response')
+table(SongsTest$Top10, SongPredict > 0.45)
+accuracy = (309+19)/nrow(SongsTest)
+
+
