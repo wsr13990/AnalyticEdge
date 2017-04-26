43ea9b4a3d6c109b135069bdf4df50c3878b8448
unknown <wahyuros@PB90048.telkomsel.co.id>
2017-04-26 13:49:43 +0700

    Finished Assigment2 Week6

--- week6/Assigment2.R
+++ week6/Assigment2.R
@@ -13,3 +13,17 @@ plot(airlineHier)
 clusterGroup = cutree(airlineHier, k=5)
 table(clusterGroup)
 
+tapply(airline$Balance, clusterGroup, mean)
+tapply(airline$QualMiles, clusterGroup, mean)
+tapply(airline$BonusMiles, clusterGroup, mean)
+tapply(airline$BonusTrans, clusterGroup, mean)
+tapply(airline$FlightMiles, clusterGroup, mean)
+tapply(airline$FlightTrans, clusterGroup, mean)
+tapply(airline$DaysSinceEnroll, clusterGroup, mean)
+
+set.seed(88)
+airlineKMean = kmeans(airlinesNorm, centers = 5)
+table(airlineKMean$cluster)
+
+
+table(clusterGroup, airlineKMean$cluster)
